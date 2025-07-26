a7826f262e9f185848ebe57e214c597d98b5508a
Satish Kumar <satishsjce86@gmail.com>
2025-07-26 11:44:56 +0000

    Advance Multi Processing Output

---
+++ 16-Multithreading and Multiprocessing/16.1-advance_multi_processing-output.lua
@@ -0,0 +1,17 @@
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>python 16.1-advance_multi_processing.py
+Square: 1
+Square: 4
+Square: 9
+Square: 16
+Square: 25
+Square: 36
+Square: 49
+Square: 64
+Square: 81
+Square: 121
+Square: 4
+Square: 9
+Square: 144
+Square: 196
+
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>
