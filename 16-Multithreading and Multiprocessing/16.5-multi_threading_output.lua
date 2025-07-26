0eb7253dba3d9637c98da5d4a54dde686c5788b1
Satish Kumar <satishsjce86@gmail.com>
2025-07-26 11:58:25 +0000

    Multi Threading Output

---
+++ 16-Multithreading and Multiprocessing/16.5-multi_threading_output.lua
@@ -0,0 +1,14 @@
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>python 16.5-multi_threading.py
+Number:0
+Letter: a
+Number:1
+Letter: b
+Number:2
+Letter: c
+Letter: d
+Number:3
+Number:4
+Letter: e
+10.006314039230347
+
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>
