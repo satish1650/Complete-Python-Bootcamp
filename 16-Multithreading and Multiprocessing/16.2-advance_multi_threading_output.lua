c4cdaedff5d7120e15238a9fcf7abab33d99816f
Satish Kumar <satishsjce86@gmail.com>
2025-07-26 11:46:19 +0000

    Advance Multi Threading Output

---
+++ 16-Multithreading and Multiprocessing/16.2-advance_multi_threading_output.lua
@@ -0,0 +1,16 @@
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>python 16.2-advance_multi_threading.py
+Number :1
+Number :2
+Number :3
+Number :4
+Number :5
+Number :6
+Number :7
+Number :8
+Number :9
+Number :0
+Number :1
+Number :2
+Number :3
+
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>
