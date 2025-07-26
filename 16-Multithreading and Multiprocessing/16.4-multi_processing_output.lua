6281ced63fd970b7bfcf6e0a73587cfeda9a52f4
Satish Kumar <satishsjce86@gmail.com>
2025-07-26 11:51:20 +0000

    Multi Processing Output

---
+++ 16-Multithreading and Multiprocessing/16.4-multi_processing_output.lua
@@ -0,0 +1,14 @@
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>python 16.4-multi_processing.py
+Square: 0
+Cube: 0
+Square: 1
+Cube: 1
+Square: 4
+Square: 9
+Cube: 8
+Square: 16
+Cube: 27
+Cube: 64
+7.689590930938721
+
+(C:\Satish\Complete-Python-Bootcamp\venv) C:\Satish\Complete-Python-Bootcamp\16-Multithreading and Multiprocessing>
